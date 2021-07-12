4160d98d499b83c0acef95638f000efc3fdee762
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-07-12 23:18:20 +0530

    submit-edit-btn-working

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ const App = () => {
   const [name,setName] = useState('')
   const [email,setEmail] = useState('')
   const [number,setNumber] = useState('')
+  const [submit,setSubmit] = useState(false)
+  const [edit,setEdit] = useState(true)
   
   const handleChange = (e) => {
-  
+    
+    setSubmit(false)
     if(e.target.type == 'text') {
   
       setName(e.target.value)
@@ -20,14 +23,55 @@ const App = () => {
       setNumber(e.target.value)
     }
   }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSubmit(true)
+    setEdit(false)
+  } 
+  const editForm = (e) => {
+    e.preventDefault()
+    setEdit(true)
+    setSubmit(false)
+  }
   return (
     <div className="App">
-      <input type='text' placeholder='Name...' onChange={handleChange} value={name} />
-      <input type='email' placeholder='Email...' onChange={handleChange} value={email} />
-      <input type='number' placeholder='PhoneNo...' onChange={handleChange} value={number} />
-      <p>{name}</p>
-      <p>{email}</p>
-      <p>{number}</p>
+      <form onSubmit={handleSubmit} >
+        <input 
+        type='text' 
+        placeholder='Name' 
+        onChange={handleChange} 
+        value={name}
+        className={`${edit ? 'show-input' : 'hide-input'}`}
+        />
+        <input 
+        type='email' 
+        placeholder='Email' 
+        onChange={handleChange} 
+        value={email}
+        className={`${edit ? 'show-input' : 'hide-input'}`}
+         />
+        <input 
+        type='number' 
+        placeholder='PhoneNo' 
+        onChange={handleChange} 
+        value={number}
+        className={`${edit ? 'show-input' : 'hide-input'}`}
+         />
+        <button type='submit'>submit</button>
+        <button onClick={editForm}>edit</button>
+      </form>
+
+      <div className={`${submit ? 'show-form' : 'hide-form'}`}>
+        <div>
+          Name: {name}
+        </div>
+        <div>
+          Email: {email}
+        </div>
+        <div>
+          Phone: {number}
+        </div>
+      </div>
     </div>
   );
 }
